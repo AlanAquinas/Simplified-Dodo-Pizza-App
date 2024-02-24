b3df59e31c72a3fb1b4c0dd7bbaddf4fbd6b70ff
Arslan <alan.aquinasss@gmail.com>
2024-02-25 00:16:01 +0600

    correct img put in pizza active

--- app/src/main/java/com/example/dodo_clone_lab1/ComboActivity.kt
+++ app/src/main/java/com/example/dodo_clone_lab1/ComboActivity.kt
@@ -7,16 +7,29 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ComboActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_combo)
+//
+//        val title: TextView = findViewById(R.id.pizza_list_title)
+//        val description: TextView = findViewById(R.id.pizza_list_desc)
+//
+//
+//        title.text = intent.getStringExtra("pizzaTitle")
+//        description.text = intent.getStringExtra("pizzaDesc")
+//
+//        val floatingBackButton: FloatingActionButton = findViewById(R.id.floating_back_button)
+//
+//        floatingBackButton.setOnClickListener {
+//            onBackPressed()
+//        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_combo)
 
         val title: TextView = findViewById(R.id.pizza_list_title)
-        val sizeDesc: TextView = findViewById(R.id.pizza_list_size_desc)
         val description: TextView = findViewById(R.id.pizza_list_desc)
 
 
         title.text = intent.getStringExtra("pizzaTitle")
-        sizeDesc.text = intent.getStringExtra("sizeDesc")
         description.text = intent.getStringExtra("pizzaDesc")
 
         val floatingBackButton: FloatingActionButton = findViewById(R.id.floating_back_button)
